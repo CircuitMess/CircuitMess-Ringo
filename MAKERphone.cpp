d0c6c03bdd03eeaad77d58ef80ecf5567dd8c9a0
CircuitMess <31940328+CircuitMess@users.noreply.github.com>
2018-10-13 19:53:04 +0200

    License added in code

--- MAKERphone.cpp
+++ MAKERphone.cpp
@@ -1,3 +1,22 @@
+/*
+This file is part of the MAKERphone library,
+Copyright (c) CircuitMess 2018-2019
+This is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License (LGPL)
+as published by the Free Software Foundation, either version 3 of
+the License, or (at your option) any later version.
+This is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Lesser General Public License (LGPL) for more details.
+You should have received a copy of the GNU Lesser General Public
+License (LGPL) along with the library.
+If not, see <http://www.gnu.org/licenses/>.
+Authors:
+ - Albert Gajsak
+ - Emil Gajsak
+*/
+
 #include "MAKERphone.h"
 extern MAKERphone mp;
 void MAKERphone::begin() {
