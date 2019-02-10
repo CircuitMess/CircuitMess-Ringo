d54040458144ee2b7784ee6f08b870caacfceaec
MiLeG <emil.gajsak@gmail.com>
2019-02-10 14:57:52 +0100

    Edited collision functions

--- src/MAKERphone.cpp
+++ src/MAKERphone.cpp
@@ -7152,27 +7152,18 @@ uint16_t Buttons::timeHeld(uint8_t button) {
 
 //Collision
 bool MAKERphone::collideRectRect(int16_t x1, int16_t y1, int16_t w1, int16_t h1, int16_t x2, int16_t y2, int16_t w2, int16_t h2) {
-	return (x2 < x1 + w1 && x2 + w2 > x1
-		&& y2 < y1 + h1 && y2 + h2 > y1);
+	return (x2 < x1 + w1 && x2 + w2 > x1 && y2 < y1 + h1 && y2 + h2 > y1);
 }
 bool MAKERphone::collidePointRect(int16_t pointX, int16_t pointY, uint16_t rectX, uint16_t rectY, uint16_t rectW, uint16_t rectH) {
 	return (pointX >= rectX && pointX < rectX + rectW && pointY >= rectY && pointY < rectY + rectH);
 }
 bool MAKERphone::collideCircleCircle(int16_t centerX1, int16_t centerY1, int16_t r1, int16_t centerX2, int16_t centerY2, int16_t r2) {
-	// True if the distance between the centers is smaller than the sum of the radii : dist < r1 + r2
-	// dist = sqrt( (centerX1 - centerX2)^2 + (centerY1 - centerY2)^2 ) but we can't use sqrt (too slow).
-	// however, sqrt(a) < b  <==>  a < b*b  for all natural numbers
-	// in our case, 'a' is by definition a natural numbers
-	// So let's make sure 'b' is a natural numbers
 	r1 = abs(r1);
 	r2 = abs(r2);
-	// Now apply the formula
 	return (((centerX1 - centerX2) * (centerX1 - centerX2) + (centerY1 - centerY2) * (centerY1 - centerY2)) < (r1 + r2) * (r1 + r2));
 }
 bool MAKERphone::collidePointCircle(int16_t pointX, int16_t pointY, int16_t centerX, int16_t centerY, int16_t r) {
-	// Just like circleCircle collision, but r2 = 0
-	r = abs(r);
-	return (((pointX - centerX) * (pointX - centerX) + (pointY - centerY) * (pointY - centerY)) < r * r);
+	return (((pointX - centerX) * (pointX - centerX) + (pointY - centerY) * (pointY - centerY)) < abs(r) ^ 2);
 }
 
 //SD operations
